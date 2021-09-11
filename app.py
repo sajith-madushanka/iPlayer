3321308c96db027e0a3eb09965c48ab54ec93904
sajith_madusanka <lankathilakarokz@gmail.com>
2021-09-11 22:31:41 +0530

    songs in one folder

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 import numpy as np
 import cv2
 from keras.models import load_model
+import fnmatch
 import os
-import random
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './static/songs/'
@@ -32,13 +32,13 @@ def uploadfile():
     lang= request.form['lang']
     file = request.files['file']
     filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], lang+'/'+mood+'/'+filename))
+    name = filename[:-4]
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], name+'.'+lang+'.'+mood+'.mp3'))
     return render_template('index.html')
 
 @app.route('/emotion_detect', methods=["POST"])
 def emotion_detect():
     language= request.form['language']
-    print(language)
     found = False
     cap = cv2.VideoCapture(0)
     while not found:
@@ -54,19 +54,27 @@ def emotion_detect():
     roi = roi/255.0
     roi = np.reshape(roi, (1, 48, 48, 1))
     prediction = model.predict(roi)
-    print(prediction)
     prediction = np.argmax(prediction)
     prediction = label_map[prediction]
 
     cap.release()
-
-    song1= random.choice(os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"+language+"/"+prediction+"/"))
-    song2= random.choice(os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"+language+"/"+prediction+"/"))
-    song3= random.choice(os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"+language+"/"+prediction+"/"))
-    song4= random.choice(os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"+language+"/"+prediction+"/"))
-    song5= random.choice(os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"+language+"/"+prediction+"/"))
-    print('You are '+prediction+' !!!! please calm down:) ,I will play song for you :' + song1)
-    
+    i=1
+    for file in os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"):
+        if i==6:
+            break
+        if fnmatch.fnmatch(file, '*.'+language+'.'+prediction+'.mp3'):
+            if i==1:
+                song1= file
+            if i==2:
+                song2= file
+            if i==3:
+                song3= file
+            if i==4:
+                song4= file
+            if i==5:
+                song5= file
+            i+=1
+        
     return render_template("emotion_detect.html", data=prediction,lang=language, s1=song1, s2=song2, s3=song3, s4=song4, s5=song5)
 
 
