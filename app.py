c9e1085e5d41ddd6821f61b1a30cbe0ce2f36708
sajith-madushanka <lankathilakarokz@gmail.com>
2021-11-25 12:04:02 +0530

    updating

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import cv2
 from keras.models import load_model
 import fnmatch
 import os
-from werkzeug.utils import secure_filename
+import random
 
-UPLOAD_FOLDER = './static/songs/'
+
+UPLOAD_FOLDER = './static/'
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -26,19 +27,19 @@ def index():
 def home():
     return render_template('index.html')
 
-@app.route('/',methods=['GET','POST'])
-def uploadfile():
-    mood= request.form['mood']
-    lang= request.form['lang']
-    file = request.files['file']
-    filename = secure_filename(file.filename)
-    name = filename[:-4]
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], name+'.'+lang+'.'+mood+'.mp3'))
-    return render_template('index.html')
+# @app.route('/',methods=['GET','POST'])
+# def uploadfile():
+#     mood= request.form['mood']
+#     lang= request.form['lang']
+#     file = request.files['file']
+#     filename = secure_filename(file.filename)
+#     name = filename[:-4]
+#     file.save(os.path.join(app.config['UPLOAD_FOLDER'], name+'.'+lang+'.'+mood+'.mp3'))
+#     return render_template('index.html')
 
 @app.route('/emotion_detect', methods=["POST"])
 def emotion_detect():
-    language= request.form['language']
+    type= request.form['type']
     found = False
     cap = cv2.VideoCapture(0)
     while not found:
@@ -59,21 +60,23 @@ def emotion_detect():
 
     cap.release()
     i=1
-    for file in os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"):
-        if i==6:
-            break
-        if fnmatch.fnmatch(file, '*.'+language+'.'+prediction+'.mp3'):
-            if i==1:
-                song1= file
-            if i==2:
-                song2= file
-            if i==3:
-                song3= file
-            if i==4:
-                song4= file
-            if i==5:
-                song5= file
-            i+=1
+    if prediction != 'Neutral':
+        song1 = random.choice(os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/" + language + "/" + prediction + "/"))
+    # for file in os.listdir("F:/gg/ppp/emotion-based-music-ai-main/static/songs/"):
+    #     if i==6:
+    #         break
+    #     if fnmatch.fnmatch(file, '*.'+language+'.'+prediction+'.mp3'):
+    #         if i==1:
+    #             song1= file
+    #         if i==2:
+    #             song2= file
+    #         if i==3:
+    #             song3= file
+    #         if i==4:
+    #             song4= file
+    #         if i==5:
+    #             song5= file
+    #         i+=1
         
     return render_template("emotion_detect.html", data=prediction,lang=language, s1=song1, s2=song2, s3=song3, s4=song4, s5=song5)
 
